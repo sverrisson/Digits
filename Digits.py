5d353f102fc4f20ae1bdb2425965c5b824dce6a7
Hannes Sverrisson <hannessv@gmail.com>
2025-07-17 12:32:50 +0200

    Digits plotted

--- Digits.py
+++ Digits.py
@@ -7,20 +7,20 @@ import matplotlib.pyplot as plt
 # uv pip install numpy matplotlib
 # uv run Digits.py
 
-line = 0
-with open('semeion.data', mode='r') as file:
-    data_reader = csv.reader(file, delimiter=' ')
-    for row in data_reader:
-        image = np.array(row[:256]).reshape(16,16)
-        digit = np.array(row[256:-1])
-        if line < 1:
-            print(image)
-            print(digit)
-        line += 1
-
-
 def show_image(image: list):
     a = image
     plt.imshow(a)
     plt.axis('off')  # Turn off axis labels
     plt.show()
+
+line = 0
+with open('semeion.data', mode='r') as file:
+    data_reader = csv.reader(file, delimiter=' ')
+    for row in data_reader:
+        image = np.array(row[:256], dtype=float).reshape(16,16)
+        digit = np.array(row[256:-1])
+        if line < 4:
+            print(image)
+            print(digit)
+            show_image(image)
+        line += 1
