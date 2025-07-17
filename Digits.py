06c2f751bd25ae4fe5a4c8a4a3ade99415182389
Hannes Sverrisson <hannessv@gmail.com>
2025-07-17 12:24:09 +0200

    importing training data

--- Digits.py
+++ Digits.py
@@ -2,21 +2,21 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 
-line = 0
-with open('semeion.data', mode='r') as file:
-    data_reader = csv.reader(file)
-    for row in data_reader:
-        if line < 1:
-            print(row)
-        line += 1
-
-
 # uv venv
 # source .venv/bin/activate
 # uv pip install numpy matplotlib
 # uv run Digits.py
-# uv pip install yfinance --upgrade
-# ulimit -n 4096
+
+line = 0
+with open('semeion.data', mode='r') as file:
+    data_reader = csv.reader(file, delimiter=' ')
+    for row in data_reader:
+        image = np.array(row[:256]).reshape(16,16)
+        digit = np.array(row[256:-1])
+        if line < 1:
+            print(image)
+            print(digit)
+        line += 1
 
 
 def show_image(image: list):
